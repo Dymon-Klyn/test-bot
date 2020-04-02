a618ca9281d17623eb300c6c220c3278bd9c1b36
Daniel <danyalternate@gmail.com>
2020-04-02 17:25:20 -0400

    changed token

--- bot.py
+++ bot.py
@@ -1,7 +1,4 @@
-import os
 from discord.ext import commands
-
-TOKEN = os.environ.get("TOKEN")
 
 bot = commands.Bot(command_prefix="!")
 
@@ -9,4 +6,4 @@ bot = commands.Bot(command_prefix="!")
 async def hi(ctx):
     await ctx.send("hello")
 
-bot.run(TOKEN)
+bot.run("Njc5ODYwNzgxOTUyNzk0NjQ2.XoZSUg.pb_WsuYSS810czto-9oMlPohvWw")
